387ecfa2f2e8f8de940dc4d1cff5b208844b5ba3
ChekeredList71 <naszpeti@gmail.com>
2022-08-06 17:43:42 +0200

    Small changes
    ActionListener changed to lambda, window now appears in the middle of the screen, added "Success" dialogs, dealt with warnings, formatted.

--- src/src/App.java
+++ src/src/App.java
@@ -1,19 +1,11 @@
 package src;
 
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Locale;
-
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) {
@@ -46,40 +38,23 @@ public class App {
 
         f.setSize(455, 200);
         f.setLayout(null);
+        f.setLocationRelativeTo(null);
         f.setVisible(true);
 
         // on press
-        startV.addActionListener(new ActionListener() {
+        startV.addActionListener(e -> startVanguard());
 
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                startVanguard();
-            }
-        });
+        stopV.addActionListener(e -> stopVanguard());
 
-        stopV.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                stopVanguard();
-            }
-        });
-
-        killV.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                killVanguard();
-            }
-        });
+        killV.addActionListener(e -> killVanguard());
     }
 
     public static void startVanguard() {
 
         // check for admin
-        if (!WindowsAdminUtil.isUserWindowsAdmin()) { // TODO: Test startVanguard() with admin rights
+        if (WindowsAdminUtil.isUserWindowsAdmin()) { // TODO: Test startVanguard() with admin rights
             try {
-                String[] commands = { "sc config vgc start= demand", "sc config vgk start= system" };
+                String[] commands = {"sc config vgc start= demand", "sc config vgk start= system"};
                 cmdRunner.run(commands);
 
             } catch (IOException e) {
@@ -87,7 +62,8 @@ public class App {
                 JOptionPane.showMessageDialog(f, "IOException occurred lmao", "Error", JOptionPane.ERROR_MESSAGE);
                 System.exit(0);
             }
-            if (JOptionPane.showConfirmDialog(f, "Vanguard will start on next powerup. Restart now?", "Done",
+            //Ask if user wants to restart
+            if (JOptionPane.showConfirmDialog(f, "Vanguard will start on next powerup. Restart now?", "Success",
                     JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
                 try {
                     Process proc = Runtime.getRuntime().exec("shutdown -r -t 0");
@@ -96,7 +72,6 @@ public class App {
                     JOptionPane.showMessageDialog(f, "IOException occurred lmao", "Error", JOptionPane.ERROR_MESSAGE);
                     System.exit(0);
                 }
-
             }
         } else {
             JOptionPane.showMessageDialog(f, "This command cannot run without admin rights. Restart app as admin.",
@@ -108,9 +83,10 @@ public class App {
     public static void stopVanguard() {
         if (WindowsAdminUtil.isUserWindowsAdmin()) { // TODO: Test stopVanguard() with admin rights
             try {
-                String[] commands = { "sc config vgc start= disabled", "sc config vgk start= disabled", "net stop vgc",
-                        "net stop vgk", "taskkill /IM vgtray.exe" };
+                String[] commands = {"sc config vgc start= disabled", "sc config vgk start= disabled", "net stop vgc",
+                        "net stop vgk", "taskkill /IM vgtray.exe"};
                 cmdRunner.run(commands);
+                JOptionPane.showMessageDialog(f, "Vanguard process is terminated, and it won't start next time.", "Success", JOptionPane.INFORMATION_MESSAGE);
 
             } catch (IOException e) {
 
@@ -127,10 +103,11 @@ public class App {
     public static void killVanguard() {
         if (WindowsAdminUtil.isUserWindowsAdmin()) {
             try {
-                String[] commands = { "net stop vgc",
-                        "net stop vgk", "taskkill /IM vgtray.exe" };
+                String[] commands = {"net stop vgc",
+                        "net stop vgk", "taskkill /IM vgtray.exe"};
 
                 cmdRunner.run(commands);
+                JOptionPane.showMessageDialog(f, "Vanguard process is terminated.", "Success", JOptionPane.INFORMATION_MESSAGE);
             } catch (IOException e) {
 
                 JOptionPane.showMessageDialog(f, "IOException occurred lmao", "Error", JOptionPane.ERROR_MESSAGE);
@@ -141,20 +118,21 @@ public class App {
                     "Error", JOptionPane.ERROR_MESSAGE);
         }
 
+        getVanguardStatus();
     }
 
     public static String getVanguardStatus() {
 
-        ArrayList<String> output = new ArrayList<String>();
+        ArrayList<String> output = new ArrayList<>();
         try {
             Runtime rt = Runtime.getRuntime();
-            String[] commands = { "sc", "query", "vgk" };
+            String[] commands = {"sc", "query", "vgk"};
             Process proc = rt.exec(commands);
 
             BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
 
             // Read the output from the command
-            String s = null;
+            String s;
             while ((s = stdInput.readLine()) != null) {
                 output.add(s);
             }
@@ -175,9 +153,12 @@ public class App {
             else if (Integer.parseInt(output.get(3).substring(29, 30)) == 1)
                 return "STOPPED";
         } catch (StringIndexOutOfBoundsException e) {
+            //noinspection UnnecessarySemicolon
             ;
         }
 
-        return "failed to check aaaaaaaaaa";
+        return "failed to check";
     }
 }
+
+//TODO: check for Windows platform
